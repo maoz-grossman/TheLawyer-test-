989a56de833a600b1996fa95efc7187d5374c0de
maoz-grossman <maozg2008@gmail.com>
2019-12-10 23:25:04 +0200

    initialization of the Game

---
+++ New Unity Project (2)/Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Animator _anim;
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float direction = Input.GetAxis("Horizontal");
+        _anim.SetFloat("Move", direction);
+        //...
+    }
+}
